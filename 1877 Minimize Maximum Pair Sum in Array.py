ba9c83376bd1c56f4673594c1c27c3fe78cb8698
Sri Harsha Thatha <91465207+sriharsha1718@users.noreply.github.com>
2026-01-25 09:20:59 +0530

    Update 1877 Minimize Maximum Pair Sum in Array.py

--- 1877 Minimize Maximum Pair Sum in Array.py
+++ 1877 Minimize Maximum Pair Sum in Array.py
@@ -6,6 +6,7 @@ class Solution:
         for i in range(n//2):
             max_sum = max(max_sum, nums[i] + nums[n - i - 1])
         return max_sum
-        # Time Complexity: O(N Log N)
+        # Time Complexity: O(N Log N) because of sort() method
         # Space Complexity: O(N)
+
         # Python uses Timsort which uses extra memory proportional to N
