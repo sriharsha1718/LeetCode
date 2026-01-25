0dd471b77fbc705534254049dece3815a30856b0
Sri Harsha Thatha <91465207+sriharsha1718@users.noreply.github.com>
2026-01-25 09:23:32 +0530

    Add files via upload

---
+++ 1984 Minimum Difference Between Highest and Lowest of K Scores.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        max_sum = 0
+        for i in range(n//2):
+            max_sum = max(max_sum, nums[i] + nums[n - i - 1])
+        return max_sum
+        # Time Complexity: O(N Log N)
+        # Space Complexity: O(N)
+        # Python uses Timsort which uses extra memory proportional to N
